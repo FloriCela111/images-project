d0e137cf84c1b66278befa2be780d8c972d044a5
Daniel Cela <floricela999@gmail.com>
2023-11-14 21:45:03 +0100

    llaqi

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import dataJson from './response.json' assert {type: 'json'};
+import dataJson from "./response.json" assert { type: "json" };
 
 console.log(dataJson);
 
@@ -8,5 +8,18 @@ console.log(dataJson);
 // const heartBtn = document.querySelector('.heart-btn');
 // const authorName = document.querySelector('.author-name');
 
-
-
+const html = `
+<div class="card">
+        <div class="images">
+          <img src="${original}" alt="" class="pictures" />
+        </div>
+        <div class="subscription">
+          <div class="subscription-row">
+            <button class="heart-btn">
+              <img src="images/heart-solid.svg" alt="" />
+            </button>
+            <h1 class="author-name">Name of author</h1>
+          </div>
+        </div>
+      </div>
+`;
