d9e1423f4cd09ed563c24312fb0928226920b1c3
Flori Cela <floricela999@gmail.com>
2023-11-17 09:01:48 -0700

    llaqi

--- script.js
+++ script.js
@@ -1,93 +1,72 @@
-import dataJson from "./response.json" assert { type: "json" };
-
+// import dataJson from "./response.json" assert { type: "json" };
 //console.log(dataJson);
-
 
 const cardContainer = document.getElementById("card-container");
 cardContainer.innerHTML = "";
-
 const modalHtml = `
 <div id="myModal" class="modal">
   <span class="close"><img  src="/images/circle-xmark-solid.svg" class="closeImg"/></span>
   <img class="modal-content" id="img01" style="background-color: transparent; border-color: transparent">
   <div id="caption"></div>
 </div>
-
 `;
-
 cardContainer.insertAdjacentHTML("beforeend", modalHtml);
 
-dataJson.photos.map((photo) => {
-  const likedPhotos = photo.liked
-    ? "images/heart-solid.svg"
-    : "images/heart-regular.svg";
-  let html = `
-  <div class="card">
-        <img class="images" id='imageCard'src="${photo.src.original}">
-       
-       
-          <div class="subscription">
-            <div class="subscription-row">
-              <button class="heart-btn">
-                <img src="${likedPhotos}" alt="${photo}" />
-              </button>
-              <h1 class="author-name">${photo.photographer}</h1>
-            </div>
-          </div>
-  </div>
-  `;
-
-  // const modal = document.getElementById('myModal');
-  // // const caption = document.getElementById('caption');
-  // const images = document.getElementById("image")
-  // const image = document.getElementById('img01');
-  // const close = document.querySelector('close');
-  // const card = document.querySelector("card")
-
-  cardContainer.insertAdjacentHTML("beforeend", html);
-
-});
-
-// let divImage = document.getElementById("card-container").children
-// console.log(divImage)
-// divImage.forEach(element => {
-//   element.addEventListener('click', function (e) {
-//     document.getElementById("modal").classList.add("modal")
-//     console.log('Button clicked', e);
-//     // modal.classList.remove('hidden');
-//     // caption.classList.remove('hiddden');
-
-//   })
+// dataJson.photos.map((photo) => {
+//   const likedPhotos = photo.liked
+//     ? "images/heart-solid.svg"
+//     : "images/heart-regular.svg";
+//   let html = `
+//   <div class="card">
+//         <img class="images" id='imageCard'src="${photo.src.original}">
+//           <div class="subscription">
+//             <div class="subscription-row">
+//               <button class="heart-btn">
+//                 <img src="${likedPhotos}" alt="${photo}" />
+//               </button>
+//               <h1 class="author-name">${photo.photographer}</h1>
+//             </div>
+//           </div>
+//   </div>
+//   `;
+//   cardContainer.insertAdjacentHTML("beforeend", html);
 // });
+
+
 const modal = document.querySelector(".modal");
 const caption = document.getElementById('caption');
 const myModal = document.getElementById('myModal')
-
 const imagesCards = document.querySelectorAll("#imageCard")
 const modalImage = document.getElementById('img01');
 const closeModalBtn = document.querySelector(".close");
-// const close = document.querySelector('close');
-// const card = document.querySelector("card")
-
 imagesCards.forEach((image) => {
   image.addEventListener("click", function () {
     modal.style.display = "block";
     modalImage.src = this.src;
     caption.innerHTML = this.alt;
   })
-
   closeModalBtn.onclick = function () {
     modal.style.display = "none";
   };
-
   myModal.addEventListener('click', function () {
     modal.style.display = "none";
   })
   document.addEventListener('keydown', function (e) {
-
     if (e.key === "Escape") {
       modal.style.display = "none";
     }
   })
 })
 
+
+const apiUrl = "https://api.pexels.com/v1/search?query=";
+const apiKey = "563492ad6f9170000100000172e40c73ea194f8589f3de8585118b13";
+
+const checkPictures = function (pictures) {
+
+  fetch(`${apiUrl}${pictures}`, {
+    headers: {
+      'Authorization': apiKey
+    }
+  }).then(response => response.json()).then(data => console.log(data))
+}
